54db16c8d32d84a5708b671f8608389a924dc88b
Mehmet Beydogan <mbbrain@gmail.com>
2013-05-03 17:27:56 +0300

    user following and followers added

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,18 @@ class User < ActiveRecord::Base
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me, :login
   # attr_accessible :title, :body
 
+
+  has_many :items
+
+  has_many :followings, :foreign_key => "follower_id",
+           :dependent => :destroy
+  has_many :following, :through => :followings, :source => :followed
+
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+           :class_name => "Following",
+           :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
+
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
@@ -26,6 +38,18 @@ class User < ActiveRecord::Base
     else
       where(conditions).first
     end
+  end
+
+  def following?(followed)
+    followings.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+    followings.create!(:followed_id => followed.id)
+  end
+
+  def unfollow!(followed)
+    followings.find_by_followed_id(followed).destroy
   end
 
 end
