fdd07f7f22dda03b02a96ac45c60626006aefb34
Mehmet Beydogan <mbbrain@gmail.com>
2013-05-03 21:26:33 +0300

    attachments error fixed

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,5 +2,5 @@ class Attachment < ActiveRecord::Base
   attr_accessible :description, :title, :version, :attachable_id, :attachable_type, :attach
   has_attached_file :attach, :styles => { :large => '950x400#', :medium => "250x200#", :small => "100x100>" }
 
-  belongs_to :item, :foreign_key => :attachable_id
+  belongs_to :item, :foreign_key => "attachable_id"
 end
