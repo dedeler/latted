3490852786eb6fa127578e61b3d3e6dae14ef60a
Mehmet Beydogan <mbbrain@gmail.com>
2013-05-03 21:06:08 +0300

    item user

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,6 @@
 class Item < ActiveRecord::Base
   attr_accessible :content, :item_category_id, :link, :store_id, :title, :user_id
 
-  belongs_to :items
-
-  has_many :likes
-  has_many :liking_users, :through => :likes, :source => :user
+  has_many :user_actions
+  has_many :actioned, :through => :user_actions, :source => :user
 end
