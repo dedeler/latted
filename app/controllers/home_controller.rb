01d166cc36aa2a14ba119d227f367a2702fa6af6
Mehmet Beydogan <mbbrain@gmail.com>
2013-05-03 17:03:16 +0300

    root route and home controller added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @items }
+    end
   end
 end
