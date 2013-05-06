a8ff8bcab945404c4ea4742b9bb50939993fa995
Mehmet Beydogan <mbbrain@gmail.com>
2013-05-06 17:51:19 +0300

    item and item category relations added

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,8 @@ class Item < ActiveRecord::Base
   has_many :user_actions
   has_many :actioned, :through => :user_actions, :source => :user
 
+  belongs_to :item_category
+
   def thumb
     attachments.find_by_id(self.thumb_id)
   end
