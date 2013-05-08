058bd37c93f0b4d5bcfff70a31c108652bdb49cb
Mehmet Beydogan <mbbrain@gmail.com>
2013-05-08 20:13:38 +0300

    item images added to items/show

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,6 @@
 class Attachment < ActiveRecord::Base
   attr_accessible :description, :title, :version, :attachable_id, :attachable_type, :attach
-  has_attached_file :attach, :styles => { :large => '950x400#', :medium => "250x200#", :small => "100x100>" }
+  has_attached_file :attach, :styles => { :large => '950x400#', :medium => "372x370#", :small => "72x72>" }
 
   belongs_to :item, :foreign_key => "attachable_id"
 end
