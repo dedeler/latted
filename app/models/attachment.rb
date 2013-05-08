7cdc01560998e499e594076a7b2db9c2fef653eb
Mehmet Beydogan <mbbrain@gmail.com>
2013-05-08 20:27:26 +0300

    item image sizes added to application.rb as global variable

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,6 @@
 class Attachment < ActiveRecord::Base
   attr_accessible :description, :title, :version, :attachable_id, :attachable_type, :attach
-  has_attached_file :attach, :styles => { :large => '950x400#', :medium => "372x370#", :small => "72x72>" }
+  has_attached_file :attach, :styles => Sociality::Application::ITEM_IMAGE_SIZES
 
   belongs_to :item, :foreign_key => "attachable_id"
 end
