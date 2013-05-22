8d6345492525d5dd0278f037010f4f37fd9037d7
Mehmet Beydogan <mbbrain@gmail.com>
2013-05-22 03:01:05 +0300

    added home.js to compile

--- config/application.rb
+++ config/application.rb
@@ -67,5 +67,8 @@ module Sociality
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.precompile += ['home.js']
+
+
   end
 end
