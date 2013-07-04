bc3cbd538ddcd8fa3f8b8d0ce7e8e71e5e2c285b
Mehmet Beydogan <mbbrain@gmail.com>
2013-07-04 23:09:08 +0300

    pipeline for fonts created

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Sociality::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
 end
