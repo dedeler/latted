4a3dcc890a9067e0d103889b4a9a05d70412afb1
Mehmet Beydogan <mbbrain@gmail.com>
2013-07-04 22:43:00 +0300

    deployment configured for assets:precompile only if asset is changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,7 +54,21 @@ end
 
 after :deploy, "passenger:restart"
 
-set :max_asset_age, 2 ## Set asset age in minutes to test modified date against.
+set :max_asset_age, 15 ## Set asset age in minutes to test modified date against.
+
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+end
+
 #
 #after "deploy:finalize_update", "deploy:assets:determine_modified_assets", "deploy:assets:conditionally_precompile"
 #
