cd3cb190a33bec748179f0966370f9bc513747dc
Mehmet Beydogan <mbbrain@gmail.com>
2013-07-05 18:57:32 +0300

    user profile page implemented

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,40 +56,40 @@ after :deploy, "passenger:restart"
 
 set :max_asset_age, 15 ## Set asset age in minutes to test modified date against.
 
-namespace :deploy do
-  namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
-  end
-end
-
-#
-#after "deploy:finalize_update", "deploy:assets:determine_modified_assets", "deploy:assets:conditionally_precompile"
-#
 #namespace :deploy do
 #  namespace :assets do
-#
-#    desc "Figure out modified assets."
-#    task :determine_modified_assets, :roles => assets_role, :except => { :no_release => true } do
-#      set :updated_assets, capture("find #{latest_release}/app/assets -type d -name .git -prune -o -mmin -#{max_asset_age} -type f -print", :except => { :no_release => true }).split
-#    end
-#
-#    desc "Remove callback for asset precompiling unless assets were updated in most recent git commit."
-#    task :conditionally_precompile, :roles => assets_role, :except => { :no_release => true } do
-#      if(updated_assets.empty?)
-#        callback = callbacks[:after].find{|c| c.source == "deploy:assets:precompile" }
-#        callbacks[:after].delete(callback)
-#        logger.info("Skipping asset precompiling, no updated assets.")
+#    task :precompile, :roles => :web, :except => { :no_release => true } do
+#      from = source.next_revision(current_revision)
+#      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+#        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
 #      else
-#        logger.info("#{updated_assets.length} updated assets. Will precompile.")
+#        logger.info "Skipping asset pre-compilation because there were no asset changes"
 #      end
 #    end
-#
 #  end
 #end
+
+
+after "deploy:finalize_update", "deploy:assets:determine_modified_assets", "deploy:assets:conditionally_precompile"
+
+namespace :deploy do
+  namespace :assets do
+
+    desc "Figure out modified assets."
+    task :determine_modified_assets, :roles => assets_role, :except => { :no_release => true } do
+      set :updated_assets, capture("find #{latest_release}/app/assets -type d -name .git -prune -o -mmin -#{max_asset_age} -type f -print", :except => { :no_release => true }).split
+    end
+
+    desc "Remove callback for asset precompiling unless assets were updated in most recent git commit."
+    task :conditionally_precompile, :roles => assets_role, :except => { :no_release => true } do
+      if(updated_assets.empty?)
+        callback = callbacks[:after].find{|c| c.source == "deploy:assets:precompile" }
+        callbacks[:after].delete(callback)
+        logger.info("Skipping asset precompiling, no updated assets.")
+      else
+        logger.info("#{updated_assets.length} updated assets. Will precompile.")
+      end
+    end
+
+  end
+end
