0c20a5e33067b8aca95ea24816c72cce8e699063
Mehmet Beydogan <mbbrain@gmail.com>
2013-07-05 18:57:32 +0300

    user profile page implemented

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,27 @@
+$(function() {
+    $( ".tabs > li > a" ).click(function(e){
+        var target = $(this).data("tab");
+        $("#user-tabs > div:visible").fadeOut({
+            duration: 500,
+            complete: function(){
+                $("#user-tabs ." + target).fadeIn("slow");
+            }
+        });
+
+    });
+
+    $(".user-item").hover(
+        function(){
+            $(this).find(">div").not(".item-more").animate({
+                opacity: 0.25
+            }, 400);
+            $(this).find(".item-more").fadeIn("slow");
+        },
+        function(){
+            $(this).find(">div").not(".item-more").animate({
+                opacity: 1
+            }, 200);
+            $(this).find(".item-more").fadeOut();
+        }
+    )
+});
