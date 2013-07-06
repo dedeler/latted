3bc5abd243e3b3151367edb969aba8a8278f1d37
Mehmet Beydogan <mbbrain@gmail.com>
2013-07-06 22:10:19 +0300

    upgraded to rails4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Sociality::Application.config.secret_token = 'ae7d402dc02fd056c48d924fc0a7a7b6e2136471597306584cd5ca6eba854f83eee4b81b7a16437e943d995e29ef709cfda20508b3e73467a96bc27643fbd099'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4try::Application.config.secret_key_base = '7c07560dff1a47093bd4078ee85d331326e75f3dac706197c7e1843971fa72726da7f24f80cd84e12573f2b08febba0b417648a26bc03ac3ec19edee6d0fcd4c'
