d85fdba5577578a0449ab2b9aa9df805486a82a9
Mehmet Beydogan <mbbrain@gmail.com>
2013-07-06 22:30:49 +0300

    app renamed to latted

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def default_url(size)
-    "http://placehold.it/"+Sociality::Application::ITEM_IMAGE_SIZES[size.to_sym]
+    "http://placehold.it/"+Latted::Application::ITEM_IMAGE_SIZES[size.to_sym]
   end
 
 end
--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,6 @@
 class Attachment < ActiveRecord::Base
   attr_accessible :description, :title, :version, :attachable_id, :attachable_type, :attach
-  has_attached_file :attach, :styles => Sociality::Application::ITEM_IMAGE_SIZES
+  has_attached_file :attach, :styles => Latted::Application::ITEM_IMAGE_SIZES
 
   belongs_to :item, :foreign_key => "attachable_id"
 end
