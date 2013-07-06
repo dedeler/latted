916c775141c8b5f5447e1c29d6754b43c57420a4
Mehmet Beydogan <mbbrain@gmail.com>
2013-07-06 22:10:19 +0300

    upgraded to rails4

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
-# Load the rails application
+# Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# Initialize the rails application
-Sociality::Application.initialize!
+# Initialize the Rails application.
+Rails4try::Application.initialize!
