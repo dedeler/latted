71e91de498c9a284e58fe1196f3f583ec1901bc6
Mehmet Beydogan <mbbrain@gmail.com>
2013-07-06 22:30:21 +0300

    app renamed to latted

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails4try::Application.config.session_store :cookie_store, key: '_rails4try_session'
+Sociality::Application.config.session_store :cookie_store, key: '_Sociality_session'
