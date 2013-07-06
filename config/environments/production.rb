70f72d489d7031e6d188d1a717eeb50caf66997d
Mehmet Beydogan <mbbrain@gmail.com>
2013-07-06 22:03:39 +0300

    devise#session error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,4 +69,7 @@ Sociality::Application.configure do
 
   # Precompile additional assets
   config.assets.precompile += %w( .svg .eot .woff .ttf )
+
+  config.assets.initialize_on_precompile = false
+
 end
